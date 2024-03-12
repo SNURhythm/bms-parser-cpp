82306a6979dae4e589dabd1bcb1e00e19a7f084a
VioletXF <zudevxf@gmail.com>
2024-03-13 00:27:16 +0900

    Optimize makefile & Add test

--- Makefile
+++ Makefile
@@ -1,27 +1,38 @@
 CC=g++
-CCFLAGS=-std=c++17 -g -DBMS_PARSER_VERBOSE=1
+CCFLAGS=-std=c++17 -g -DBMS_PARSER_VERBOSE=0
 SRC_FILES = $(wildcard src/*.cpp)
 OBJ_PATH=obj
 BUILD_PATH=build
 OBJ_FILES = $(patsubst src/%.cpp,$(OBJ_PATH)/%.o,$(SRC_FILES))
+DEP_DIR=.deps
+DEPS = $(SRC_FILES:src/%.cpp=$(DEP_DIR)/%.d)
+DEPFLAGS=-MMD -MP -MT $@ -MF $(DEP_DIR)/$*.d
+
+-include $(DEP_FILES) # Include the dependency files
 
 all: $(OBJ_FILES)
+$(DEP_DIR):
+	@mkdir -p $(DEP_DIR)
 $(OBJ_PATH):
 	@mkdir -p $(OBJ_PATH)
 $(BUILD_PATH):
 	@mkdir -p $(BUILD_PATH)
 
-$(OBJ_PATH)/%.o: src/%.cpp $(OBJ_PATH)
-	$(CC) $(CCFLAGS) -c -o $@ $<
+$(OBJ_PATH)/%.o: src/%.cpp | $(DEP_DIR) $(OBJ_PATH)
+	$(CC) $(CCFLAGS) $(DEPFLAGS) -c -o $@ $<
 example/sqlite3.o: example/sqlite3.c
 	gcc -c -o example/sqlite3.o example/sqlite3.c
 example: all example/sqlite3.o $(BUILD_PATH)
 	$(CC) $(CCFLAGS) -o $(BUILD_PATH)/main example/main.cpp example/sqlite3.o $(OBJ_FILES)
 amalgamate: $(BUILD_PATH)
 	python3 scripts/amalgamate.py $(BUILD_PATH)/bms_parser.hpp $(SRC_FILES)
-test_amalgamation: amalgamate example/sqlite3.o $(BUILD_PATH)
-	@cp $(BUILD_PATH)/bms_parser.hpp example/
-	$(CC) $(CCFLAGS) -DWITH_AMALGAMATION=1 -o $(BUILD_PATH)/test_amalgamation example/main.cpp example/sqlite3.o
-	./$(BUILD_PATH)/test_amalgamation example/example.bme
+test_amalgamation: amalgamate $(BUILD_PATH)
+	@cp $(BUILD_PATH)/bms_parser.hpp test/
+	$(CC) $(CCFLAGS) -DWITH_AMALGAMATION=1 -o test/test_amalgamation test/main.cpp
+	cd test && ./test_amalgamation
+test: all $(BUILD_PATH)
+	$(CC) $(CCFLAGS) -o test/test test/main.cpp $(OBJ_FILES)
+	cd test && ./test
 clean:
-	@rm -rf $(OBJ_PATH) $(BUILD_PATH)
+	@rm -rf $(OBJ_PATH) $(BUILD_PATH) $(DEP_DIR) test/test test/test_amalgamation
+
---
+++ test/main.cpp
@@ -0,0 +1,181 @@
+#include <string>
+#include <filesystem>
+#include <atomic>
+#include <fstream>
+#include <iostream>
+#if WITH_AMALGAMATION
+#include "bms_parser.hpp"
+#else
+#include "../src/Parser.h"
+#include "../src/Chart.h"
+#endif
+
+#define ASSERT_EQ(a, b, desc) \
+    if (a != b)               \
+    {                         \
+        std::cerr << desc << std::endl;    \
+        std::cerr << "\tExpected: " << a << std::endl; \
+        std::cerr << "\tActual: " << b << std::endl; \
+        return 1;             \
+    } else { \
+        std::cout << "\t" << desc << " passed" << std::endl; \
+    }
+#define ASSERT_EQW(a, b, desc) \
+    if (a != b)               \
+    {                         \
+        std::cerr << desc;    \
+        std::wcerr << "\tExpected: " << a << std::endl; \
+        std::wcerr << "\tActual: " << b << std::endl; \
+        return 1;             \
+    } else { \
+        std::cout << "\t" << desc << " passed" << std::endl; \
+    }
+std::string ws2s(const std::wstring &wstr)
+{
+  return std::string().assign(wstr.begin(), wstr.end());
+}
+int main()
+{
+    // read inputs from ./testcases/*.bme
+    std::vector<std::filesystem::path> inputs;
+    for (auto &p : std::filesystem::directory_iterator("./testcases"))
+    {
+        if (p.path().extension() == ".bme")
+        {
+            inputs.push_back(p.path());
+        }
+    }
+
+    for (auto &input : inputs)
+    {
+        std::filesystem::path output_path = input;
+        output_path.replace_extension(".output");
+        if (std::filesystem::exists(output_path))
+        {
+            std::cout << "Testing " << input << "..." << std::endl;
+            bms_parser::Chart *chart;
+            std::atomic_bool cancel = false;
+            bms_parser::Parser parser;
+            parser.Parse(input.wstring(), &chart, false, false, cancel);
+            std::wifstream ifs(output_path);
+            std::wstring line;
+            while (std::getline(ifs, line))
+            {
+                if (line.rfind(L"md5: ", 0) == 0)
+                {
+                    auto out = line.substr(5);
+                    ASSERT_EQ(ws2s(out), chart->Meta.MD5, "md5: ");
+                }
+                else if (line.rfind(L"sha256: ", 0) == 0)
+                {
+                    auto out = line.substr(8);
+                    ASSERT_EQ(ws2s(out), chart->Meta.SHA256, "sha256: ");
+                }
+                else if (line.rfind(L"title: ", 0) == 0)
+                {
+                    auto out = std::wstring(line.begin() + 7, line.end());
+                    ASSERT_EQW(out, chart->Meta.Title, "title: ");
+                }
+                else if (line.rfind(L"artist: ", 0) == 0)
+                {
+                    auto out = std::wstring(line.begin() + 8, line.end());
+                    ASSERT_EQW(out, chart->Meta.Artist, "artist: ");
+                }
+                else if (line.rfind(L"genre: ", 0) == 0)
+                {
+                    auto out = std::wstring(line.begin() + 7, line.end());
+                    ASSERT_EQW(out, chart->Meta.Genre, "genre: ");
+                }
+                else if (line.rfind(L"subartist: ", 0) == 0)
+                {
+                    auto out = std::wstring(line.begin() + 11, line.end());
+                    ASSERT_EQW(out, chart->Meta.SubArtist, "subartist: ");
+                }
+                else if (line.rfind(L"total: ", 0) == 0)
+                {
+                    auto out = std::stod(line.substr(7));
+                    ASSERT_EQ(out, chart->Meta.Total, "total: ");
+                }
+                else if (line.rfind(L"total_notes: ", 0) == 0)
+                {
+                    auto out = std::stoi(line.substr(13));
+                    ASSERT_EQ(out, chart->Meta.TotalNotes, "total_notes: ");
+                }
+                else if (line.rfind(L"total_backspin_notes: ", 0) == 0)
+                {
+                    auto out = std::stoi(line.substr(22));
+                    ASSERT_EQ(out, chart->Meta.TotalBackSpinNotes, "total_backspin_notes: ");
+                }
+                else if (line.rfind(L"total_long_notes: ", 0) == 0)
+                {
+                    auto out = std::stoi(line.substr(18));
+                    ASSERT_EQ(out, chart->Meta.TotalLongNotes, "total_long_notes: ");
+                }
+                else if (line.rfind(L"total_scratch_notes: ", 0) == 0)
+                {
+                    auto out = std::stoi(line.substr(21));
+                    ASSERT_EQ(out, chart->Meta.TotalScratchNotes, "total_scratch_notes: ");
+                }
+                else if (line.rfind(L"min_bpm: ", 0) == 0)
+                {
+                    auto out = std::stod(line.substr(9));
+                    ASSERT_EQ(out, chart->Meta.MinBpm, "min_bpm: ");
+                }
+                else if (line.rfind(L"max_bpm: ", 0) == 0)
+                {
+                    auto out = std::stod(line.substr(9));
+                    ASSERT_EQ(out, chart->Meta.MaxBpm, "max_bpm: ");
+                }
+                else if (line.rfind(L"bpm: ", 0) == 0)
+                {
+                    auto out = std::stod(line.substr(5));
+                    ASSERT_EQ(out, chart->Meta.Bpm, "bpm: ");
+                }
+                else if (line.rfind(L"minbpm: ", 0) == 0)
+                {
+                    auto out = std::stod(line.substr(8));
+                    ASSERT_EQ(out, chart->Meta.MinBpm, "minbpm: ");
+                }
+                else if (line.rfind(L"maxbpm: ", 0) == 0)
+                {
+                    auto out = std::stod(line.substr(8));
+                    ASSERT_EQ(out, chart->Meta.MaxBpm, "maxbpm: ");
+                }
+                else if (line.rfind(L"is_dp: ", 0) == 0)
+                {
+                    auto out = line.substr(7) == L"true";
+                    ASSERT_EQ(out, chart->Meta.IsDP, "is_dp: ");
+                }
+                else if (line.rfind(L"key_mode: ", 0) == 0)
+                {
+                    auto out = std::stoi(line.substr(10));
+                    ASSERT_EQ(out, chart->Meta.KeyMode, "key_mode: ");
+                }
+                else if (line.rfind(L"difficulty: ", 0) == 0)
+                {
+                    auto out = std::stoi(line.substr(12));
+                    ASSERT_EQ(out, chart->Meta.Difficulty, "difficulty: ");
+                }
+                else if (line.rfind(L"playlevel: ", 0) == 0)
+                {
+                    auto out = std::stoi(line.substr(11));
+                    ASSERT_EQ(out, chart->Meta.PlayLevel, "playlevel: ");
+                }
+                else if (line.rfind(L"player: ", 0) == 0)
+                {
+                    auto out = std::stoi(line.substr(8));
+                    ASSERT_EQ(out, chart->Meta.Player, "player: ");
+                }
+                else if (line.rfind(L"rank: ", 0) == 0)
+                {
+                    auto out = std::stoi(line.substr(6));
+                    ASSERT_EQ(out, chart->Meta.Rank, "rank: ");
+                }
+            }
+            delete chart;
+            std::cout << "\tPass" << std::endl;
+        }
+    }
+
+    return 0;
+}
